f4a17afb78defe0a126c846964bcd9de77a34225
Serhiy <mcseruy@gmail.com>
2016-09-25 22:08:50 +0300

    add MANIFEST file

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.04',
     packages=find_packages,
     install_requires=['Flask', 'Pillow'],
+    include_package_data=True,
     url='https://github.com/Serhiy-Lishchuk/Myblog',
     license='Free',
     author='serhiy',
