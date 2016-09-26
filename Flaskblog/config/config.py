985c7f28c6ef6f05ce999b379490c255957bc880
Serhiy <mcseruy@gmail.com>
2016-09-26 23:01:40 +0300

    add logger

--- Flaskblog/config/config.py
+++ Flaskblog/config/config.py
@@ -18,3 +18,8 @@ UPLOAD_IMAGE = os.path.join(APP_DIR, config.get('PATH', 'UPLOAD_IMAGE'))
 IMAGE_EXTENSIONS = set(['jpg', 'png'])
 MAX_WIDTH = int(config.get('OTHER', 'MAX_WIDTH'))
 WATERMARK = os.path.join(APP_DIR, config.get('PATH', 'WATERMARK'))
+
+LOG_NAME = config.get('LOG', 'NAME')
+LOG_FILE = config.get('LOG', 'FILENAME')
+LOG_LEVEL = config.get('LOG', 'LEVEL')
+LOG_FORMAT = config.get('LOG', 'FORMAT')
